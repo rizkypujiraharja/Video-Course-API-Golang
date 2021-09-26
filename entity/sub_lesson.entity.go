b118a43fc298cc826481f75fb409c79044310d6f
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-09-26 18:02:36 +0700

    fix error salah type

--- entity/sub_lesson.entity.go
+++ entity/sub_lesson.entity.go
@@ -3,6 +3,6 @@ package entity
 type SubLesson struct {
 	ID       int64   `gorm:"primary_key:auto_increment" json:"-"`
 	Title    string  `gorm:"type:varchar(255)" json:"-"`
-	LessonID uint    `gorm:"not null" json:"-"`
+	LessonID int64   `gorm:"not null" json:"-"`
 	Videos   []Video `json:"-"`
 }
